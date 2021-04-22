eb609fa5964f533d2127708894a304fd5321cfb6
Storm K <asge0907@easv365.dk>
2021-04-22 17:17:54 +0200

    Added next auth

---
+++ src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,22 @@
+import { NextApiHandler } from 'next';
+import NextAuth from 'next-auth';
+import Adapters from 'next-auth/adapters';
+import Providers from 'next-auth/providers';
+import db from '../../../utils/db';
+
+const authHandler: NextApiHandler = (req, res) => NextAuth(req, res, options);
+export default authHandler;
+
+const options = {
+  providers: [
+    Providers.GitHub({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
+    }),
+  ],
+  adapter: Adapters.Prisma.Adapter({
+    prisma: db,
+  }),
+
+  secret: process.env.SECRET,
+};
