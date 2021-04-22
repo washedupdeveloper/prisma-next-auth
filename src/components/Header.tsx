d4bb3a3c01b2ac70b1e7820c66cd0049b924f09d
Storm K <asge0907@easv365.dk>
2021-04-22 17:17:11 +0200

    modified base components

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,6 @@
 import { Flex, Heading } from '@chakra-ui/react';
 
-export const Header = ({ title }: { title: string }) => (
+export const Header = ({ title }: { title?: string }) => (
   <Flex justifyContent="center" alignItems="center" height="100vh" bgGradient="linear(to-l, #7928CA, #FF0080)" bgClip="text">
     <Heading fontSize="6vw">{title}</Heading>
   </Flex>
